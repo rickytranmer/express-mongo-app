e22e7b77ff35d44606bd56e5315a89312e9ebf8e
rickytranmer <rwtranmer@gmail.com>
2017-12-30 12:45:14 -0700

    adds update function

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var mongo = require("mongodb").MongoClient;
-var prompt = require("prompt-sync")();
+var prompt = require("prompt-sync")();		// DON'T USE NODEMON //
 var url = "mongodb://localhost:27017/restaurant_db";
 
 mongo.connect(url, function(err, db) {
 	var collection = db.collection('restaurants');
   
   	// - Show all restaurants if user types 'all'
+  	//
 	// var allChoice = prompt("Type 'all' and press enter to display all restaurants' names: ");
 	// if (allChoice == "all") {
 	//   collection.find().toArray(function(err, doc) {
@@ -16,33 +17,49 @@ mongo.connect(url, function(err, db) {
 	// }
 
 	// - Find restaurant by name
+	//
 	// var nameChoice = prompt("Enter a restaurant name:");
-	// collection.find({name: nameChoice}).forEach(function(doc) {
+	// collection.find( {name: nameChoice} ).forEach(function(doc) {
 	// 	console.log(doc);
 	// });
 
 	// - Create a prompt to add restaurant
-	var newName = prompt("Enter a restaurant name:");
-	var newStreet = prompt("Enter the street:");
-	var newZip = prompt("Enter the ZIP code:");
-	var newYelp = prompt("Enter the yelp listing:");
-	var newRestaurant = {
-		name: newName,
-		address: {
-			street: newStreet,
-			zipcode: newZip
-		},
-		yelp: newYelp
-	};
-	collection.insert( newRestaurant );
+	//
+	// var newName = prompt("Enter a restaurant name:");
+	// var newStreet = prompt("Enter the street:");
+	// var newZip = prompt("Enter the ZIP code:");
+	// var newYelp = prompt("Enter the yelp listing:");
+	// var newRestaurant = {
+	// 	name: newName,
+	// 	address: {
+	// 		street: newStreet,
+	// 		zipcode: newZip
+	// 	},
+	// 	yelp: newYelp
+	// };
+	// collection.insert( newRestaurant );
 
-	//TODO - Create a prompt for users to edit restaurant
-	// var allChoice = prompt("Enter a restaurant name:");
-	// collection.find({name: allChoice}).forEach(function(doc) {
-	// 	console.log(doc);
-	// });
+	// - Create a prompt for users to edit restaurant
+	//
+	var editChoice = prompt("Enter a restaurant name to edit:");
+	collection.find({name: editChoice}).toArray(function(err, doc) {
+		if (err) {console.log(err)};
+		var editName = prompt("Enter its NEW restaurant name:");
+		var editStreet = prompt("Enter its NEW street:");
+		var editZip = prompt("Enter its NEW ZIP code:");
+		var editYelp = prompt("Enter its NEW yelp listing:");
+		doc[0].name = editName;
+		doc[0].address.street = editStreet;
+		doc[0].address.zipcode = editZip;
+		doc[0].yelp = editYelp;
+		console.log(doc);
+		collection.update({name: editChoice}, doc[0], function(err, res) {
+			err ? console.log(err) : console.log(res.result);
+		});
+	});
 
 	//TODO - Create a prompt for users to delete restaurants.
+	//
 	// var allChoice = prompt("Enter a restaurant name:");
 	// collection.find({name: allChoice}).forEach(function(doc) {
 	// 	console.log(doc);
